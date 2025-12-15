58fa947cd058830ac0e94384f84267a782698a4c
eStella_ <75822584+doot00@users.noreply.github.com>
2025-12-15 21:12:28 +0900

    email
    .

--- gopet/src/app/components/main/Footer.tsx
+++ gopet/src/app/components/main/Footer.tsx
@@ -4,7 +4,7 @@ const Footer = () => {
         <>
             <footer className="fixed bottom-0 left-0 w-full bg-black text-white py-8">
                 <div className="mx-auto flex justify-center items-center">
-                    <span>Email : ass1023@naver.com</span>
+                    <span>Email : tmxpffk03@naver.com</span>
                 </div>
             </footer>
         </>
